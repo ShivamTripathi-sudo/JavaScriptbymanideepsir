3464e829e9be589572e09bcb42a1819422e6558f
ShivamTripathi-sudo <shivamtripathi6060@gmail.com>
2024-09-23 18:39:26 +0530

    done

---
+++ basic.js
@@ -0,0 +1,54 @@
+var a= "hi good after noon";
+console.log(a);
+var username =["sanny","lokesh", "hunny","ram" ,"sappa"];
+console.log(username[0]);
+username[4]="pradip"
+console.log(username[4])
+console.log(username)
+var user=[123,345,543,671,213,]
+console.log(user[2]);
+console.log(user);
+var us=['shysm','hari'];
+console.log(us);
+// var employeeId=['varun','sowtware developer',8865,'hyderabad'];
+console.log(employeeId );
+username[4]="shivam tripathi";
+console.log(username);
+username[5]="keshav tripathi"; 
+var employeeId={
+    name:'varun kumar rathaur',
+    designation:'software engineer',
+    salary:40000,
+    loaction: 'hyderbad',
+    address:{
+        flatno: '302b2',
+        appartment:'aprna',
+        area: 'madhaour',
+        city:'hyderbad',
+
+}
+}
+console.log(' name:' , employeeId['name']);
+console.log('name :', employeeId.name);
+console.log('salary:',employeeId.salary)
+
+console.log("adress:", employeeId.address.appartment);
+console.log(employeeId.designation) ; 
+console.log(employeeId.address.city);
+var employeeId={
+    mbs:{
+mobile1:7868785768,
+mobile2:785648939
+
+    },
+
+mobile:[6790-373737,675-764-7878]
+}
+
+console.log("mobilenumber :",employeeId.mobile[1])
+console.log("mobile no first:",employeeId.mbs.mobile1)
+function enrollNow(){
+    console.log("hi buddy how are you")
+
+}
+enrollNow();
